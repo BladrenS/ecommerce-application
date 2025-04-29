9ceedf9732c0ff9123dfc9f51a27187a538fb6b5
[BladrenS] <denis_ka_98@mail.ru>
2025-04-29 17:31:18 +0600

    feat: update jest optimization

--- jest.config.cjs
+++ jest.config.cjs
@@ -11,4 +11,14 @@ module.exports = {
     '\\.(css|less|scss)$': 'identity-obj-proxy'
   },
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/build/'],
+  reporters: [
+    'default',
+    [ 'jest-html-reporter', {
+      pageTitle: 'Test Report',
+      outputPath: 'test-report/index.html',
+      includeFailureMsg: true,
+      includeConsoleLog: true
+    }]
+  ]
 };
