8e5f214fea68263a6881e54934836c3def35cf28
Constantine Saveliev <senior.saveliev2012@yandex.ru>
2025-05-18 08:58:47 +0300

    refactor: delete useless file

--- src/api/credentials.ts
+++
@@ -1,9 +0,0 @@
-export const credentials = {
-  CTP_PROJECT_KEY: 'rs-desperados',
-  CTP_CLIENT_SECRET: 'tH0_TH29tLClb3gh884h38IjY34Ahskk',
-  CTP_CLIENT_ID: 'eBGlEmnCf31h0pDuGVpe2TFM',
-  CTP_AUTH_URL: 'https://auth.eu-central-1.aws.commercetools.com',
-  CTP_API_URL: 'https://api.eu-central-1.aws.commercetools.com',
-  CTP_SCOPES:
-    'view_categories:rs-desperados view_tax_categories:rs-desperados view_cart_discounts:rs-desperados view_standalone_prices:rs-desperados manage_orders:rs-desperados manage_shopping_lists:rs-desperados view_products:rs-desperados manage_order_edits:rs-desperados view_types:rs-desperados view_sessions:rs-desperados manage_customers:rs-desperados view_project_settings:rs-desperados view_discount_codes:rs-desperados create_anonymous_token:rs-desperados view_published_products:rs-desperados manage_sessions:rs-desperados view_product_selections:rs-desperados view_shipping_methods:rs-desperados',
-};
