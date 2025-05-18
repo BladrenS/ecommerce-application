3b74f97358a608660aca5b5b1e9497c3241058a0
Constantine Saveliev <senior.saveliev2012@yandex.ru>
2025-05-18 15:08:59 +0300

    feat: add country code mapper

--- src/constants/countries.ts
+++ src/constants/countries.ts
@@ -9,3 +9,15 @@ export const validCountries = [
   'Lithuania',
   'Spain',
 ]; // Сroatia, Estonia, Finland, France, Germany, Greece, Italy, Lithuania, Spain
+
+export const countryNameToCode: Record<string, string> = {
+  Croatia: 'HR',
+  Estonia: 'EE',
+  Finland: 'FI',
+  France: 'FR',
+  Germany: 'DE',
+  Greece: 'GR',
+  Italy: 'IT',
+  Lithuania: 'LT',
+  Spain: 'ES',
+};
