856289ce8d047e4164035c986d7304a7dbee1504
Constantine Saveliev <senior.saveliev2012@yandex.ru>
2025-05-19 10:00:38 +0300

    feat: make Button universal component

--- src/components/Ui/Button/Button.tsx
+++ src/components/Ui/Button/Button.tsx
@@ -2,9 +2,11 @@ import type { ComponentProps, FC, PropsWithChildren } from 'react';
 
 import styles from './styles.module.scss';
 
-export const Button: FC<PropsWithChildren<ComponentProps<'button'>>> = ({ children, ...props }) => {
+export const Button: FC<PropsWithChildren<ComponentProps<'button'>>> = ({ children, className, ...props }) => {
+  const combinedClassName = [styles.button, className].filter(Boolean).join(' ');
+
   return (
-    <button className={styles.button} {...props}>
+    <button className={combinedClassName} {...props}>
       {children}
     </button>
   );
